0a709d172ac116ffaf86caa22d6ffd6dd0fb4f92
DonutLord20 <kaibayston@gmsil.com>
2025-11-01 20:09:06 +0000

    added CharacterClasses

---
+++ CharacterClasses.cs
@@ -0,0 +1,11 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game_off_2025;
+
+public class Character
+{
+    protected Texture2D _SpriteSheet;
+    protected Rectangle _CurrentFrame;
+}
